dda08931e61b3e190d50abc1ad7aed3f787148d8
Jonathan Havstad <jjhavstad@gmail.com>
2016-10-08 16:30:19 -0600

    Adding refresh layout and refresh button

--- app/src/androidTest/java/com/loopcupcakes/examples/bluetoothleexample/ApplicationTest.java
+++ app/src/androidTest/java/com/loopcupcakes/examples/bluetoothleexample/ApplicationTest.java
@@ -1,13 +1,69 @@
 package com.loopcupcakes.examples.bluetoothleexample;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.test.ApplicationTestCase;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResource() {
+        Espresso.registerIdlingResources(mActivityTestRule.getActivity().idlingResource);
+    }
+
+    @Test
+    public void testScanLEBluetooth() {
+        onView(withId(R.id.scan_le_button)).check(matches(isEnabled()));
+        onView(withId(R.id.scan_le_button)).perform(click());
+        onView(withId(R.id.scan_le_button)).check(matches(isEnabled()));
+    }
+
+    @Test
+    public void testRefreshScanLEBluetooth() {
+        onView(withId(R.id.refresh_layout)).perform(swipeDown());
+        onView(withId(R.id.refresh_layout)).check(new ViewAssertion() {
+            @Override
+            public void check(View view, NoMatchingViewException noViewFoundException) {
+                if (((SwipeRefreshLayout) view).isRefreshing()) {
+                    throw new AssertionError("SwipeRefreshLayout is refreshing!");
+                }
+            }
+        });
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        Espresso.unregisterIdlingResources(mActivityTestRule.getActivity().idlingResource);
     }
 }
--- app/src/main/java/com/loopcupcakes/examples/bluetoothleexample/MainActivity.java
+++ app/src/main/java/com/loopcupcakes/examples/bluetoothleexample/MainActivity.java
@@ -11,12 +11,16 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.test.espresso.idling.CountingIdlingResource;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewPropertyAnimator;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -25,7 +29,7 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
     private static final long SCAN_PERIOD = 10 * 1000;
@@ -34,33 +38,57 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
     private BluetoothAdapter mBluetoothAdapter;
     private Handler mHandler;
     private BluetoothLeScanner mBluetoothScanner;
-    private ArrayAdapter<String> mArrayAdapter;
+    private BluetoothLEAdapter mArrayAdapter;
 
     private ArrayList<BluetoothDevice> mBluetoothDevices;
 
+    @Bind(R.id.refresh_layout)
+    SwipeRefreshLayout mRefreshLayout;
+
     @Bind(R.id.list_view_1)
-    ListView mListView;
+    RecyclerView mListView;
+
+    @Bind(R.id.text_view1)
+    TextView mTextView;
+
+    @Bind(R.id.scan_le_button)
+    Button mScanLEButton;
+
+    ViewPropertyAnimator mViewPropertyAnimator;
+
+    public CountingIdlingResource idlingResource = new CountingIdlingResource("BlueToothLEButton");
 
     private ScanCallback mLeScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
             Log.d(TAG, "onScanResult: " + result.toString());
+            mTextView.setText("Received scan result");
+            mRefreshLayout.setRefreshing(false);
             BluetoothDevice bluetoothDevice = result.getDevice();
-            mArrayAdapter.add(bluetoothDevice.getAddress() + " " + bluetoothDevice.getName());
+            mArrayAdapter.notifyDataSetChanged();
             mBluetoothDevices.add(bluetoothDevice);
+            mScanLEButton.setEnabled(true);
+            idlingResource.decrement();
         }
 
         @Override
         public void onBatchScanResults(List<ScanResult> results) {
             super.onBatchScanResults(results);
             Log.d(TAG, "onBatchScanResults: " + results.toString());
+            mTextView.setText("Received batch scan results");
+            mRefreshLayout.setRefreshing(false);
+            mScanLEButton.setEnabled(true);
+            idlingResource.decrement();
         }
 
         @Override
         public void onScanFailed(int errorCode) {
             super.onScanFailed(errorCode);
             Log.d(TAG, "onScanFailed: " + errorCode);
+            mTextView.setText("Scan failed");
+            mScanLEButton.setEnabled(true);
+            idlingResource.decrement();
         }
     };
 
@@ -81,12 +109,22 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         setListView();
 
         scanLeDevice(true);
+
+        setupRefreshView();
+
+        setupScanLEButton();
     }
 
     private void setListView() {
-        mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        mArrayAdapter = new BluetoothLEAdapter(mBluetoothDevices,
+                new BluetoothLEAdapter.OnBluetoothItemClickHandler() {
+
+                    @Override
+                    public void onBluetoothItemClicked(int position) {
+                        MainActivity.this.onBluetoothItemClicked(position);
+                    }
+                });
         mListView.setAdapter(mArrayAdapter);
-        mListView.setOnItemClickListener(this);
     }
 
     private void scanLeDevice(final boolean enable) {
@@ -95,10 +133,19 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
                 @Override
                 public void run() {
                     mBluetoothScanner.stopScan(mLeScanCallback);
+                    mTextView.setText("No results");
+                    mScanLEButton.setEnabled(true);
+                    mRefreshLayout.setRefreshing(false);
+                    idlingResource.decrement();
+                    Toast.makeText(MainActivity.this, "Stopped scanning", Toast.LENGTH_SHORT).show();
                 }
             }, SCAN_PERIOD);
 
             mBluetoothScanner.startScan(mLeScanCallback);
+            mTextView.setText("Scanning...");
+            mScanLEButton.setEnabled(false);
+            idlingResource.increment();
+            Toast.makeText(this, "Scanning for LE Bluetooth", Toast.LENGTH_LONG).show();
         } else {
             mBluetoothScanner.startScan(mLeScanCallback);
         }
@@ -124,12 +171,51 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         }
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    private void onBluetoothItemClicked(int position) {
         Intent intent = new Intent(this, DeviceActivity.class);
 
         intent.putExtra(DeviceActivity.EXTRA_DEVICE_KEY, mBluetoothDevices.get(position));
 
         startActivity(intent);
+    }
+
+    private void setupRefreshView() {
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                MainActivity.this.scanLeDevice(true);
+            }
+        });
+    }
+
+    private void setupScanLEButton() {
+        mViewPropertyAnimator = mScanLEButton.animate();
+        mScanLEButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return handleScanLEButtonClick(v, event);
+            }
+        });
+    }
+
+    private boolean handleScanLEButtonClick(View view, MotionEvent event) {
+        long translationZDuration =
+                getResources().getInteger(R.integer.custombutton_translationz_duration);
+        int translationZ = getResources().getInteger(R.integer.custombutton_translationz);
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mScanLEButton.setPressed(true);
+            mViewPropertyAnimator
+                    .setDuration(translationZDuration)
+                    .translationZBy(translationZ)
+                    .start();
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            mScanLEButton.setPressed(false);
+            mViewPropertyAnimator
+                    .setDuration(translationZDuration)
+                    .translationZBy(-translationZ)
+                    .start();
+            scanLeDevice(true);
+        }
+        return false;
     }
 }
